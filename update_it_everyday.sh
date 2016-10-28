afa06e291e1f579684ba6a11ed41210479455b9d
RainX <1020682330@qq.com>
2016-10-28 14:15:01 +0800

    add change

--- update_it_everyday.sh
+++ update_it_everyday.sh
@@ -4,10 +4,14 @@
 ## 周一至周五每天晚上23点更新 
 ## 0 23 * * 1,2,3,4,5 path to update_it_everyday.sh
 
+
+
 # go to dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
+# pull first
+git pull
 
 # gen files
 gen-all-index-benchmark-data $DIR/data
